e73bfdb7d7aba42136fc6f3326d3038a1a610307
Dudely72 <jakobhall2000@gmail.com>
2025-03-23 17:37:46 -0500

    added basic while loop to keep screen black

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():        #this for loop makes the close button work
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, "black")    #constantly keeps screen black
+        pygame.display.flip()                   #refreshes display
 
 if __name__ == "__main__":
     main()
